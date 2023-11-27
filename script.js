1465fa62034bb7d6b1296cd9e924f27db8215bb3
Victoria <61987205+vickyluvs@users.noreply.github.com>
2023-11-26 22:29:33 -0500

    Created a function to flip profile image

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
   let information = document.querySelector(".information");
   information.addEventListener("click", showProjectDetails);
 });
+
+//flip image function
+function flipCoin() {
+  var image = document.querySelector(".profile-photo");
+  if (image.src.endsWith("vicky.png")) {
+    image.src = "./resources/images/undrawillustrate1.png";
+  } else {
+    image.src = "./resources/images/vicky.png";
+  }
+}
